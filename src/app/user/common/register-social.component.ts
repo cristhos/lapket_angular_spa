0857166ca440083e1c00f780e4993443ec4a48a1
Lithos Cristal <cristallithos@gmail.com>
2017-02-13 16:22:47 +0200

    add material-animation

--- src/app/user/common/register-social.component.ts
+++ src/app/user/common/register-social.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 
 export class RegisterSocialComponent {
 
+  //besoin de contacter notre app facebook 
+  //après validation redirection sur notre page d'aceuiil
+  facebookConnect(){
+   
+  }
+  //besoin de contacter notre app google
+  //après validation redirection sur notre page d'acceuil
+  googleConnect(){
+    
+  }
+
 }
