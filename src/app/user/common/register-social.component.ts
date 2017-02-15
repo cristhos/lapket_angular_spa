b293f8ead10811d1536eebd13292b7dc749cbe2d
Lithos Cristal <cristallithos@gmail.com>
2017-02-15 16:14:40 +0200

    reorganise facebook init and google function

--- src/app/user/common/register-social.component.ts
+++ src/app/user/common/register-social.component.ts
@@ -10,7 +10,8 @@ declare let FB: any;
 })
 
 export class RegisterSocialComponent {
-  gauth: any;
+  user : any;
+  gauth:any;
   infos = {
     "google": {
       "clientId": "GOOGLE_CLIENT_ID"
@@ -27,18 +28,26 @@ export class RegisterSocialComponent {
   //enregistrer si ce la premiere fois, connecter site ce la Neme fois
   facebookConnect(){
     this.initFacebookConnect(this.infos['facebook']);
-    
-    let user = [];
-    
-    this.userService.facebookConnect(user).subscribe(
-        data =>{
-          //connexion avec rapide avec facebook
-        },
-        error => console.log(error),
-        () =>{
-           console.log("finish");
-        }
-    );
+
+    FB.getLoginStatus(function(response) {
+                if (response.status === 'connected') {
+                    // connected
+                    alert('Already connected, redirect to login page to create token.');
+                    //document.location = "{{ url("hwi_oauth_service_redirect", {service: "facebook"}) }}";
+                } else {
+                    // not_authorized
+                    FB.login(function(response) {
+                        if (response.authResponse){
+                            
+                            alert('new connexion');
+                            //document.location = "{{ url("hwi_oauth_service_redirect", {service: "facebook"}) }}";
+                        } else {
+                            alert('Cancelled.');
+                        }
+                    }, {scope: 'email'});
+                }
+    });
+
   }
  
   //recuperer les informations fourni par notre app google
@@ -46,55 +55,69 @@ export class RegisterSocialComponent {
   googleConnect(){
 
     this.initGoogleConnect(this.infos['google']);
-    
-    let user = [];
-    
-    this.userService.googleConnect(user).subscribe(
-        data =>{
-          //connexion rapide avec google
-        },
-        error => console.log(error),
-        () =>{
-           console.log("finish");
-        }
-    );
-  }
 
-  initFacebookConnect(info){
-    let d = document, fbJs, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-    fbJs = d.createElement('script');
-    fbJs.id = id;
-    fbJs.async = true;
-    fbJs.src = "//connect.facebook.net/en_US/sdk.js";
-
-    fbJs.onload = function() {
-      FB.init({
-          appId: info["clientId"],
-          status: true,
-          cookie: true,
-          xfbml: true,
-          version: info["apiVersion"]
-      });
-    };
-    ref.parentNode.insertBefore(fbJs, ref);
-  }
-
-  initGoogleConnect(info){
-    let d = document, gJs, ref = d.getElementsByTagName('script')[0];
-    gJs = d.createElement('script');
-    gJs.async = true;
-    gJs.src = "//apis.google.com/js/platform.js";
-
-    gJs.onload = function() {
-        gapi.load('auth2', function() {
-            gapi.auth2.init({
-                client_id: info["clientId"],
-                scope: 'email'
-            })
-       })
+    if (typeof(this.gauth) == "undefined"){
+        this.gauth = gapi.auth2.getAuthInstance();
     }
+    this.gauth.signIn().then(() => {
+        let profile = this.gauth.currentUser.get().getBasicProfile();
+        let idToken = this.gauth.currentUser.get().getAuthResponse().id_token;
+        this.user = {
+                      token: idToken,
+                      uid: profile.getId(),
+                      name: profile.getName(),
+                      email: profile.getEmail(),
+                      image: profile.getImageUrl(),
+                      provider: "google"
+                    };                        
+    })
+    
+  }
 
-    ref.parentNode.insertBefore(gJs, ref);
+  //load facebooksdk and initialise facebook app
+  initFacebookConnect(info){
+    
+    let d = document, fbJs, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
 
+    if(d.getElementById('facebook-jssdk') == null){
+      fbJs = d.createElement('script');
+      fbJs.id = id;
+      fbJs.async = true;
+      fbJs.src = "//connect.facebook.net/en_US/sdk.js";
+
+      fbJs.onload = function() {
+        FB.init({
+            appId: info["clientId"],
+            status: true,
+            cookie: true,
+            xfbml: true,
+            version: info["apiVersion"]
+        });
+      };
+      ref.parentNode.insertBefore(fbJs, ref);
+    }
+  }
+  
+  //load google plateformjs initialise google app
+  initGoogleConnect(info){
+    let d = document, gJs, id = 'googleplatform', ref = d.getElementsByTagName('script')[0];
+
+    if(d.getElementById('googleplatform') == null){
+      gJs = d.createElement('script');
+      gJs.id = id;
+      gJs.async = true;
+      gJs.src = "//apis.google.com/js/platform.js";
+
+      gJs.onload = function() {
+          gapi.load('auth2', function() {
+              gapi.auth2.init({
+                  client_id: info["clientId"],
+                  scope: 'email'
+              })
+        })
+      }
+
+      ref.parentNode.insertBefore(gJs, ref);
+    }
   }
 }
