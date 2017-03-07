661dcd144bda6bdc3ea30536546db60fdc4eb1dd
Lithos Cristal <cristallithos@gmail.com>
2017-03-07 02:03:45 +0200

    master prod echec

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -3,15 +3,12 @@ var webpackMerge = require('webpack-merge');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var commonConfig = require('./webpack.common.js');
 var helpers = require('./helpers');
-//const AotPlugin = require('@ngtools/webpack').AotPlugin;
-
 
 const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
 
-
 module.exports = webpackMerge(commonConfig, {
   devtool: 'source-map',
-  
+
   output: {
     path: helpers.root('dist'),
     publicPath: '/',
@@ -19,16 +16,8 @@ module.exports = webpackMerge(commonConfig, {
     chunkFilename: '[id].[hash].chunk.js'
   },
 
-  htmlLoader: {
-    minimize: false // workaround for ng2
-  },
   plugins: [
-    /*new AotPlugin({
-      tsConfigPath: '../tsconfig-aot.json',
-      entryModule: 'src/app/app.module#AppModule'
-    }),*/
-    new webpack.NoErrorsPlugin(),
-    new webpack.optimize.DedupePlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
     new webpack.optimize.UglifyJsPlugin({ // https://github.com/angular/angular/issues/10618
       mangle: {
         keep_fnames: true
@@ -39,6 +28,12 @@ module.exports = webpackMerge(commonConfig, {
       'process.env': {
         'ENV': JSON.stringify(ENV)
       }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      htmlLoader: {
+        minimize: false // workaround for ng2
+      }
     })
   ]
 });
+
